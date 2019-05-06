db32f4e875f2554891dd605ae59ae0d9c1d9b4bc
Kayley <kayley.luftig@gmail.com>
2019-05-06 15:39:10 +0000

    printing in chunks using https and get, initializing step 2 file

--- scripting-http-requests/step-1.js
+++ scripting-http-requests/step-1.js
@@ -1,10 +1,17 @@
-function getAndPrintHTMLChunks () {
+var https = require('https');
 
-  var requestOptions = {
+var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
   };
 
-  /* Add your code here */
-
+function getAndPrintHTMLChunks (requestOptions) {
+  https.get(requestOptions, function(response){
+    // set utf encoding
+    response.setEncoding('utf8');
+    response.on('data', function(data){
+      console.log('Chunk Recieved. -->', + "\n" + data);
+    });
+  });
 }
+getAndPrintHTMLChunks(requestOptions);
