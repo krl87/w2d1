6ec084c9540d263b1f317870f28e9cc971c0f940
Kayley <kayley.luftig@gmail.com>
2019-05-06 16:50:24 +0000

    making step 5 into a module and calling it with an export/require

---
+++ scripting-http-requests/step-5.js
@@ -0,0 +1,26 @@
+var https = require('https');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+module.exports = function getHTML (options, callback) {
+  var htmlFile = "";
+  https.get(options, function(response) {
+    // set utf encoding
+    response.setEncoding('utf8');
+    response.on('data', function(data){
+      htmlFile += data;
+    });
+
+    response.on('end', function(){
+      callback(htmlFile);
+    });
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
